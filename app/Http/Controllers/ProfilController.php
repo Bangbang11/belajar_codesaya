6b689472e939dfcf95e556d1b664e6d765b4a9ae
Bangbang11 <bangbanggrt@gmail.com>
2018-11-09 14:39:00 +0700

    commit kedua

--- app/Http/Controllers/ProfilController.php
+++ app/Http/Controllers/ProfilController.php
@@ -8,7 +8,7 @@ class ProfilController extends Controller
 {
     public function index() {
     	$nama = "Bangbang";
-    	$umur = 25;
+    	$umur = 23;
     	return view('profil')->with(['nama'=>$nama,
     		'umur'=>$umur]);
     }
